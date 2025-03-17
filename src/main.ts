b0edab5b05dae707c8f431c3eb10eb5cf16829e8
Raul Santamaria <raulsantamariao@hotmail.com>
2025-03-17 10:00:27 -0500

    refactor: remove unused environment variables and logging for cleaner code

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import type { MicroserviceOptions } from '@nestjs/microservices';
 import { RpcException, Transport } from '@nestjs/microservices';
 
-import { envs } from '../config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger(envs.HSM_BE_CORE_USERS_NAME);
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
       options: {
         host: '0.0.0.0',
-        port: envs.HSM_BE_CORE_USERS_PORT,
       },
     },
   );
@@ -31,6 +27,5 @@ async function bootstrap() {
     }),
   );
   await app.listen();
-  logger.log(`Microservice is active on port ${envs.HSM_BE_CORE_USERS_PORT}`);
 }
 void bootstrap();
